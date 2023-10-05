b55886ae1515dcfb0eacf81308623351d514edcb
Chase jarvis <chaseajarvis@gmail.com>
2023-10-05 19:01:09 -0400

    finished section 200 -2002-2001

--- pages/script-seating.js
+++ pages/script-seating.js
@@ -38,6 +38,32 @@ function showSectionImage(sectionId) {
 "section-204-3": "/images/seating-images/section204-3.png",
 "section-204-4": "/images/seating-images/section204-4.png",
 
+"section-202-1": "/images/seating-images/section202-1.png",
+"section-202-2": "/images/seating-images/section202-2.png",
+"section-202-3": "/images/seating-images/section202-3.png",
+"section-202-4": "/images/seating-images/section202-4.png",
+
+"section-201-1": "/images/seating-images/section201-1.png",
+"section-201-2": "/images/seating-images/section201-2.png",
+"section-201-3": "/images/seating-images/section201-3.png",
+"section-201-4": "/images/seating-images/section201-4.png",
+
+"section-203-1": "/images/seating-images/section203-1.png",
+"section-203-2": "/images/seating-images/section203-2.png",
+"section-203-3": "/images/seating-images/section203-3.png",
+"section-203-4": "/images/seating-images/section203-4.png",
+
+"section-205-1": "/images/seating-images/section205-1.png",
+"section-205-2": "/images/seating-images/section205-2.png",
+"section-205-3": "/images/seating-images/section205-3.png",
+"section-205-4": "/images/seating-images/section205-4.png",
+
+"section-200-1": "/images/seating-images/section200-1.png",
+"section-200-2": "/images/seating-images/section200-2.png",
+"section-200-3": "/images/seating-images/section200-3.png",
+"section-200-4": "/images/seating-images/section200-4.png",
+"section-200-5": "/images/seating-images/section200-5.png",
+
     };
   
     const sectionImage = sectionImageMap[sectionId];
